f218c9a4052e1cc0c839eec9b5a972d197f37806
KelvinPardinho <82275352+KelvinPardinho@users.noreply.github.com>
2025-07-02 10:10:22 -0300

    Update settings.py
    Ajuste de dominio

--- PDFProva/settings.py
+++ PDFProva/settings.py
@@ -128,5 +128,5 @@ STATICFILES_DIRS = [
 # Configuração para arquivos de mídia (uploads de usuários) - para o futuro
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Lembre-se, isso é para desenvolvimento local
-
+ALLOWED_HOSTS = ['convertpdf.com.br', 'www.convertpdf.com.br']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
