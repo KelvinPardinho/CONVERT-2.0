a3b1be4c00a2b873a39309b74afcd585880e1730
KelvinPardinho <pardinhokelvin@gmail.com>
2025-07-04 14:09:32 -0300

    Ajuste do ultimo commit

--- PDFProva/urls.py
+++ PDFProva/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('', include('converter.urls')),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Lógica para servir mídia em produção (se ainda não estiver funcionando)
+    from django.urls import re_path
+    from django.views.static import serve
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
