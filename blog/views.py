60e0cfc1799af71f3fc5357f55441dd45b89a198
KelvinPardinho <pardinhokelvin@gmail.com>
2025-07-08 15:03:39 -0300

    Ajustes nas views do  blog

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-# blog/views.py (VERSÃO CORRIGIDA)
+# blog/views.py (VERSÃO FINAL COM CAMINHOS DE TEMPLATE CORRIGIDOS)
 
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
@@ -8,27 +8,25 @@ def blog_index(request):
     featured_post = all_posts.filter(is_featured=True).first()
     
     if featured_post:
-        # PONTO 2 CORRIGIDO: Agora mostramos todos os outros posts
         other_posts = all_posts.exclude(pk=featured_post.pk)
     else:
-        # Se não há destaque, mostramos todos os posts
         other_posts = all_posts
 
     categories = Category.objects.all()
 
     context = {
         'featured_post': featured_post,
-        'other_posts': other_posts, # Mudamos o nome da variável para maior clareza
+        'other_posts': other_posts,
         'categories': categories,
     }
     
+    # CORREÇÃO: Removido o caminho redundante do template.
     return render(request, 'blog/index.html', context)
 
-# NOVA VIEW: Para exibir um único artigo
 def detalhe_artigo(request, slug):
-    # Busca o post pelo slug ou retorna um erro 404 se não encontrar
     post = get_object_or_404(Post, slug=slug, is_published=True)
     context = {
         'post': post
     }
-    return render(request, 'templates/blog/detalhe_artigo.html', context)
+    # CORREÇÃO: Removido o caminho redundante do template.
+    return render(request, 'blog/detalhe_artigo.html', context)
